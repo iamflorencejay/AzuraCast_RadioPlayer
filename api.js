74d4e1b1cb15a7310d14d889f49822a32d364026
Joey <43940776+adoboligh1@users.noreply.github.com>
2023-03-29 02:28:59 +0000

    Update api.js

--- api.js
+++ api.js
@@ -67,7 +67,7 @@ if (audioPlayer && audioPlayer.dataset.src) {
         e.paused ? setPlayStatus() : setPauseStatus()
     }), !1)
 }
-const boxplay = "https://np.streamafrica.net/api/nowplaying_static/lofi.json";
+const boxplay = "https://api.streamafrica.net/zeno/new.php?id=tabzverz0fctv";
 
 function playerInit() {
     fetch(boxplay).then((e => e.json())).then((e => {
