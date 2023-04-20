4102d221bf597ec1df7d739ac1a0c10fa6cffbcd
iamflorencejay <florencejaymunar@proton.me>
2023-04-20 20:01:17 +0800

    added nav

--- api.js
+++ api.js
@@ -116,3 +116,12 @@ function playerInit() {
     })).catch((e => console.log(e)))
 }
 playerInit();
+
+function navBar() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
